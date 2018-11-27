259a008a9eb9b26adb102178ae96b188c4d9d8c1
ToWelie89 <sonesson8909@hotmail.com>
2018-11-27 12:12:21 +0100

    test

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+console.log('start')
 
-server.listen(80);
-console.log('Listening on port 9000');
-// WARNING: app.listen(80) will NOT work here!
-
-app.get('/', function (req, res) {
-  res.send('LOL!');
-});
+var io = require('socket.io')(80);
 
 io.on('connection', function (socket) {
-  console.log('User connected');
-  socket.emit('kekistan', { hello: 'world' });
-
-  setTimeout(function() {
-    socket.emit('kekistan', { hello: 'LOOOL' });
-  }, 3000);
+  socket.emit('kekistan', { msg: 'testtttttttttt' });
 });
