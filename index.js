0733b7862fe633081e29970b4e97b7998d8b6cdb
ToWelie89 <sonesson8909@hotmail.com>
2018-11-27 12:02:25 +0100

    update

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-var express = require('express')
-var app = express()
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+server.listen(80);
+console.log('Listening on port 9000');
+// WARNING: app.listen(80) will NOT work here!
 
-app.get('/', function(request, response) {
-  response.send({
-    msg: 'hallå där snygging ;)'
-  })
-})
+app.get('/', function (req, res) {
+  res.send('LOL!');
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+io.on('connection', function (socket) {
+  console.log('User connected');
+  socket.emit('kekistan', { hello: 'world' });
+
+  setTimeout(function() {
+    socket.emit('kekistan', { hello: 'LOOOL' });
+  }, 3000);
+});
